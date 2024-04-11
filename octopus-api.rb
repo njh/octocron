afde6deced12f92a6a52bf454432a64c1b207a4b
Nicholas Humfrey <njh@aelius.com>
2024-04-11 22:04:41 +0100

    Code style fixes

--- octopus-api.rb
+++ octopus-api.rb
@@ -1,12 +1,9 @@
-#!/usr/bin/env ruby
-
 require 'json'
 require 'net/http'
 require 'uri'
 
-
 class OctopusAPI
-  BASE_URL="https://api.octopus.energy/v1/"
+  BASE_URL = 'https://api.octopus.energy/v1/'
 
   # The Octopus API key, as found on the Developer Dashboard
   # https://octopus.energy/dashboard/developer/
@@ -20,7 +17,7 @@ class OctopusAPI
   # May be looked up using Post Code and #lookup_grid_supply_point
   attr_reader :grid_supply_point
 
-  def initialize(params={})
+  def initialize(params = {})
     params.each_pair do |key, value|
       setter = "#{key}="
       if respond_to?(setter)
@@ -28,18 +25,18 @@ class OctopusAPI
       end
     end
   end
-  
+
   def grid_supply_point=(code)
     @grid_supply_point = code.to_s[-1].upcase
   end
 
-  def account_info(account_number=nil)
+  def account_info(account_number = nil)
     account_number = @account_number if account_number.nil?
-    raise "No account number given" if account_number.nil?
+    raise 'No account number given' if account_number.nil?
 
-  	result = fetch("accounts/#{account_number}")
-  	# I suspect that there can be more than one property but lets keep things simple
-  	result[:properties].first
+    result = fetch("accounts/#{account_number}")
+    # I suspect that there can be more than one property but lets keep things simple
+    result[:properties].first
   end
 
   def product_list
@@ -47,65 +44,65 @@ class OctopusAPI
     result[:results]
   end
 
-  def lookup_grid_supply_point(postcode=nil)
+  def lookup_grid_supply_point(postcode = nil)
     if postcode.nil?
-      raise "No postcode and no account number given" if @account_number.nil?
+      raise 'No postcode and no account number given' if @account_number.nil?
+
       postcode = account_info[:postcode]
     end
-  
-  	result = fetch('industry/grid-supply-points', postcode: postcode)
-  	
-  	# I suspect that there can be more than one result but lets keep things simple
-  	if result[:count] === 0
-  	  raise "No Grid Supply Points Found"
-  	elsif result[:count] === 1
-  	  result[:results].first[:group_id]
-  	else
-  	  raise "More than one Grid Supply Point found"
-  	end
+
+    result = fetch('industry/grid-supply-points', postcode: postcode)
+
+    # I suspect that there can be more than one result but lets keep things simple
+    if result[:count] === 0
+      raise 'No Grid Supply Points Found'
+    elsif result[:count] === 1
+      result[:results].first[:group_id]
+    else
+      raise 'More than one Grid Supply Point found'
+    end
   end
-  
+
   def tarrif_to_product(code)
     parts = code.split('-')
     parts.slice(2, parts.length - 3).join('-')
   end
 
-  def tariff_path(type, product_code, rate_type='standard-unit-rates')
-   type_code = type.to_s[0].upcase
-   
-   tariff_type = if type_code == 'G'
-     'gas-tariffs'
-   elsif type_code == 'E'
-    'electricity-tariffs'
-   else
-     raise "Unknown tariff type: #{type_code}"
-   end
-   
-   tariff_code = "#{type_code}-1R-#{product_code}-#{grid_supply_point}"
-   ['products', product_code, tariff_type, tariff_code, rate_type].join('/')
+  def tariff_path(type, product_code, rate_type = 'standard-unit-rates')
+    type_code = type.to_s[0].upcase
+
+    tariff_type = if type_code == 'G'
+                    'gas-tariffs'
+                  elsif type_code == 'E'
+                    'electricity-tariffs'
+                  else
+                    raise "Unknown tariff type: #{type_code}"
+                  end
+
+    tariff_code = "#{type_code}-1R-#{product_code}-#{grid_supply_point}"
+    ['products', product_code, tariff_type, tariff_code, rate_type].join('/')
   end
 
-  def fetch(path, query={})
+  def fetch(path, query = {})
     # The Octopus API requires a trailing slash
     path += '/' unless path[-1] == '/'
+
     uri = URI.parse(BASE_URL + path)
     uri.query = URI.encode_www_form(query)
-    
-    puts "Fetching: #{uri}"
+
     response = nil
-	Net::HTTP.start(uri.host, uri.port, :use_ssl => (uri.scheme == 'https')) do |http|
-	  request = Net::HTTP::Get.new(uri)
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => (uri.scheme == 'https')) do |http|
+      request = Net::HTTP::Get.new(uri)
       if @api_key
         request.basic_auth(@api_key, '')
       end
 
-	  response = http.request(request)
-	  if response.code != '200'
-	    raise "Octopus API get failed: #{response}"
-	  end
-	end
+      response = http.request(request)
+      if response.code != '200'
+        raise "Octopus API request for #{uri} failed: #{response}"
+      end
+    end
 
-	JSON.parse(response.body, :symbolize_names => true)
+    JSON.parse(response.body, :symbolize_names => true)
   end
-
 end
--- tests/octopus-api_test.rb
+++ tests/octopus-api_test.rb
@@ -6,34 +6,35 @@ require './octopus-api.rb'
 
 class OctopusAPITest < Minitest::Test
   def test_initialize
-  	octopus = OctopusAPI.new
+    octopus = OctopusAPI.new
     assert_nil(octopus.api_key)
     assert_nil(octopus.account_number)
   end
 
   def test_initialize_with_hash
-    params = {api_key: 'foobar', 'account_number' => 'sk_xxyyzz', :grid_supply_point => :b}
-  	octopus = OctopusAPI.new(params)
+    params = { api_key: 'foobar', 'account_number' => 'sk_xxyyzz', :grid_supply_point => :b }
+    octopus = OctopusAPI.new(params)
     assert_equal('foobar', octopus.api_key)
     assert_equal('sk_xxyyzz', octopus.account_number)
     assert_equal('B', octopus.grid_supply_point)
   end
 
   def test_tarrif_to_product
-  	octopus = OctopusAPI.new
+    octopus = OctopusAPI.new
     product = octopus.tarrif_to_product('E-1R-SILVER-FLEX-22-11-25-B')
     assert_equal('SILVER-FLEX-22-11-25', product)
   end
-  
+
   def test_tariff_path_gas
-  	octopus = OctopusAPI.new(grid_supply_point: 'a')
-  	path = octopus.tariff_path(:gas, 'SILVER-23-12-06', 'standard-unit-rates')
+    octopus = OctopusAPI.new(grid_supply_point: 'a')
+    path = octopus.tariff_path(:gas, 'SILVER-23-12-06', 'standard-unit-rates')
     assert_equal('products/SILVER-23-12-06/gas-tariffs/G-1R-SILVER-23-12-06-A/standard-unit-rates', path)
   end
-  
+
   def test_tariff_path_elec
-  	octopus = OctopusAPI.new(grid_supply_point: '_B')
-  	path = octopus.tariff_path(:electricity, 'LOYAL-FIX-12M-24-03-22', 'standing-charges')
-    assert_equal('products/LOYAL-FIX-12M-24-03-22/electricity-tariffs/E-1R-LOYAL-FIX-12M-24-03-22-B/standing-charges', path)
+    octopus = OctopusAPI.new(grid_supply_point: '_B')
+    path = octopus.tariff_path(:electricity, 'LOYAL-FIX-12M-24-03-22', 'standing-charges')
+    assert_equal('products/LOYAL-FIX-12M-24-03-22/electricity-tariffs/E-1R-LOYAL-FIX-12M-24-03-22-B/standing-charges',
+                 path)
   end
 end
