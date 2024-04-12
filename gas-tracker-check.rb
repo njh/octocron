20c81bc8e18521d269be1bc8c5db17f9ed8a0944
Nicholas Humfrey <njh@aelius.com>
2024-04-12 21:46:16 +0100

    Added warnings/exit codes to the Gas Tracker check script

--- gas-tracker-check.rb
+++ gas-tracker-check.rb
@@ -9,11 +9,28 @@ config = YAML.load_file('config.yml')
 
 octopus = OctopusAPI.new(config)
 
-tracker_rate = octopus.get_single_rate_for_day(:gas, config['gas_tracker_tariff'])
-puts "Tracker (#{config['gas_tracker_tariff']}): #{tracker_rate}"
+date = Date.today
+puts "Checking Gas rates for #{date} in region #{octopus.grid_supply_point}:"
+puts
 
-variable_rate = octopus.get_single_rate_for_day(:gas, config['gas_variable_tariff'])
-puts "Variable (#{config['gas_variable_tariff']}): #{variable_rate}"
+tracker_rate = octopus.get_single_rate_for_day(:gas, config['gas_tracker_tariff'], date)
+puts "Tracker (#{config['gas_tracker_tariff']}): #{tracker_rate} p/kWh"
 
-fixed_rate = octopus.get_single_rate_for_day(:gas, config['gas_fixed_tariff'])
-puts "Fixed (#{config['gas_fixed_tariff']}): #{fixed_rate}"
+variable_rate = octopus.get_single_rate_for_day(:gas, config['gas_variable_tariff'], date)
+puts "Variable (#{config['gas_variable_tariff']}): #{variable_rate} p/kWh"
+
+fixed_rate = octopus.get_single_rate_for_day(:gas, config['gas_fixed_tariff'], date)
+puts "Fixed (#{config['gas_fixed_tariff']}): #{fixed_rate} p/kWh"
+
+puts
+
+if tracker_rate > variable_rate
+  $stderr.puts "Warning: Variable rate for Gas is cheaper than Tracker"
+  exit(1)
+elsif tracker_rate > fixed_rate
+  $stderr.puts "Warning: Fixed rate for Gas is cheaper than Tracker"
+  exit(1)
+else
+  puts "Tracker is the cheapest option."
+  exit(0)
+end
