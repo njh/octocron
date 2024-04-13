9d8bebdda26c32d77d1acfc48c15051ff68e4188
Nicholas Humfrey <njh@aelius.com>
2024-04-13 01:15:53 +0100

    Added script to list the available Octopus Energy products

---
+++ list-products.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require './octopus-api'
+
+octopus = OctopusAPI.new
+products = octopus.product_list
+
+def display_products(products, direction)
+  puts direction
+  puts '-' * direction.length
+  products.each do |product|
+    next if product[:direction].upcase != direction.upcase
+
+    puts "#{product[:code]}: #{product[:full_name]}"
+  end
+  puts
+end
+
+display_products(products, 'Import')
+display_products(products, 'Export')
