eb25de9416caa43e406d297a804f6adeee804612
Nicholas Humfrey <njh@aelius.com>
2024-04-30 20:15:17 +0100

    Round gas rates to 1 decimal place

--- gas-tracker-check.rb
+++ gas-tracker-check.rb
@@ -14,13 +14,13 @@ puts "Checking Gas rates for #{date} in region #{octopus.grid_supply_point}:"
 puts
 
 tracker_rate = octopus.get_single_rate_for_day(:gas, config['gas_tracker_tariff'], date)
-puts "Tracker (#{config['gas_tracker_tariff']}): #{tracker_rate} p/kWh"
+puts "Tracker (#{config['gas_tracker_tariff']}): #{tracker_rate.round(1)} p/kWh"
 
 variable_rate = octopus.get_single_rate_for_day(:gas, config['gas_variable_tariff'], date)
-puts "Variable (#{config['gas_variable_tariff']}): #{variable_rate} p/kWh"
+puts "Variable (#{config['gas_variable_tariff']}): #{variable_rate.round(1)} p/kWh"
 
 fixed_rate = octopus.get_single_rate_for_day(:gas, config['gas_fixed_tariff'], date)
-puts "Fixed (#{config['gas_fixed_tariff']}): #{fixed_rate} p/kWh"
+puts "Fixed (#{config['gas_fixed_tariff']}): #{fixed_rate.round(1)} p/kWh"
 
 puts
 
